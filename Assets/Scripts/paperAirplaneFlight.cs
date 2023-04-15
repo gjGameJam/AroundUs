b9ddb6debf6305bd53598060a6197033414cfd0f
Grant Benson <grantjbenson@icloud.com>
2023-04-15 18:55:24 -0400

    airplane system v1
    airplanes now destroy on obstacle or timeout. Airplane prefab has rigidbody so enemies only need collider and tag. update airplane flight ontrigenter when enemies start testing

--- Assets/Scripts/paperAirplaneFlight.cs
+++ Assets/Scripts/paperAirplaneFlight.cs
@@ -6,18 +6,29 @@ public class paperAirplaneFlight : MonoBehaviour
 {
     [SerializeField]
     private float planeSpeed, duration;
+    private Vector3 forward;
 
     void Start()
     {
-        //gets the desired direction of travel
-        //Transform Orientation = this.transform;
+        //this forward defines the direction the plane is intially pointed, which is the desired path
+        forward = transform.TransformDirection (Vector3.forward);
         //call coroutine to destroy after desired duration
         Destroy(gameObject, duration);
     }
     void FixedUpdate()
     {
-        Vector3 forward = transform.TransformDirection (Vector3.forward);
+        
+        //then the airplane travels linearly on the vector 3 direction from the start
         transform.position += forward * Time.deltaTime * planeSpeed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("AirPlaneObstacle"))
+        {
+            //if the plane hits an obstacle before enemy, destroy plane
+            Destroy(gameObject);
+        }
     }
 
 
--- Assets/Scripts/paperAirplaneShooter.cs
+++ Assets/Scripts/paperAirplaneShooter.cs
@@ -45,4 +45,5 @@ public class paperAirplaneShooter : MonoBehaviour
         cooldownTimer += Time.deltaTime;
 
     }
+
 }
