eba90c6a930ee94ede1354fffd96a9d757598519
FirebombDragon <nikolaus1204@gmail.com>
2023-04-18 21:10:28 -0400

    Possible fix to restarting bug

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 
 public class RestartGame : MonoBehaviour
 {
     public void Start()
     {
-        StartCoroutine(Restarting());
-
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            StartCoroutine(Restart());
+        }
     }
 
-    IEnumerator Restarting()
+    IEnumerator Restart()
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(0);
-
     }
 }
