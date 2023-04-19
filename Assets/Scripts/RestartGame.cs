c3a49e50ac75cafe0140569312222c16340b164f
FirebombDragon <nikolaus1204@gmail.com>
2023-04-18 20:43:23 -0400

    Game now reloads from Game Over screen

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class RestartGame : MonoBehaviour
 {
-    public void BootGame()
+    public void Start()
     {
+        StartCoroutine(Restarting());
+
+    }
+
+    IEnumerator Restarting()
+    {
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene(0);
+
     }
 }
